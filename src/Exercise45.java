b2497d0209447ad9a96e94bbea0c3a056e2c1f78
garg10may <garg10may@gmail.com>
2016-05-02 11:05:59 +0530

    corrected solution

--- src/Exercise45.java
+++ src/Exercise45.java
@@ -20,6 +20,7 @@ porygonz registeel relicanth remoraid rufflet sableye scolipede scrafty seaking
 sealeo silcoon simisear snivy snorlax spoink starly tirtouga trapinch treecko
 tyrogue vigoroth vulpix wailord wartortle whismur wingull yamask
 */
+/*
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -71,11 +72,145 @@ public class Exercise45 {
 
 		Character last_character = word.charAt(word.length() - 1);
 
-		HashSet<String> options = map.get(last_character);
+		HashSet<String> options = new HashSet<String>();
+		
+		if ( map.get(last_character)!= null) { //Java null pointer exception is just frustrating
+		
+		options.addAll(map.get(last_character));
+		
+		}
+
+		options.removeAll(chain);
+
+		if (options.isEmpty()) {
+
+			return chain;
+
+		} else {
+
+			HashSet<String> clone = (HashSet<String>) options.clone();
+
+			for (String i : clone) { // sometimes I don't know why my code
+										// works or not works
+
+				ArrayList<String> growingChain = new ArrayList<String>();
+
+				growingChain.addAll(chain);
+
+				growingChain.add(i);
+
+				allChains.add(find(growingChain));
+
+			}
+
+			ArrayList<String> maxList = Collections.max(allChains, new Comparator<ArrayList<String>>() {
+				public int compare(ArrayList<String> list1, ArrayList<String> list2) {
+					return Integer.compare(list1.size(), list2.size());
+				}
+			});
+
+			return maxList;
+
+		}
+	}
+
+	public static void main(String[] args) {
+
+		initializeHashMap();
+
+		makeHashMap();
+
+		ArrayList<ArrayList<String>> allWordsMaxes = new ArrayList<ArrayList<String>>();
+
+		for (String i : pokemonArray) {
+
+			ArrayList<String> word = new ArrayList<String>();
+
+			word.add(i);
+
+			allWordsMaxes.add(find(word));
+
+		}
+
+		ArrayList<String> longest = allWordsMaxes.stream().max(Comparator.comparingInt(ArrayList::size)).get();
+
+		System.out.println(longest);
+
+		System.out.println(longest.size());
+
+	}
+}
+
+*/
+//still not able to debug why the HashSet becomes empty for some character for no reason. 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Exercise45 {
+
+	private static String pokemon = "audino bagon baltoy banette bidoof braviary bronzor carracosta charmeleon"
+			+ " cresselia croagunk darmanitan deino emboar emolga exeggcute gabite girafarig gulpin haxorus"
+			+ " heatmor heatran ivysaur jellicent jumpluff kangaskhan kricketune landorus ledyba loudred"
+			+ " lumineon lunatone machamp magnezone mamoswine nosepass petilil pidgeotto pikachu pinsir"
+			+ " poliwrath poochyena porygon2 porygonz registeel relicanth remoraid rufflet sableye"
+			+ " scolipede scrafty seaking sealeo silcoon simisear snivy snorlax spoink starly tirtouga"
+			+ " trapinch treecko tyrogue vigoroth vulpix wailord wartortle whismur wingull yamask";
+
+	private static String[] pokemonArray = pokemon.split(" ");
+
+	private static HashMap<Character, HashSet<String>> map = new HashMap<Character, HashSet<String>>();
+
+	public static void initializeHashMap() {
+
+		for (String i : pokemonArray) {
+
+			char first_character = i.charAt(0);
+
+			map.put(first_character, new HashSet<String>());
+
+		}
+	}
+
+	public static void makeHashMap() {
+
+		for (String word : pokemonArray) {
+
+			char first_character = word.charAt(0);
+
+			map.get(first_character).add(word);
+		}
+
+	}
+
+	public static ArrayList<String> find(ArrayList<String> chain) {
+
+		ArrayList<ArrayList<String>> allChains = new ArrayList<ArrayList<String>>();
+
+		String word = chain.get(chain.size() - 1);
+
+		Character last_character = word.charAt(word.length() - 1);
+		
+		//HashSet<String> options = map.get(last_character);
+		//syntactically correct and innocous looking this is very dangerous. 
+		//Hmm after hours, here options is referencing the map set therefore any operation 
+		//on it will bring changes to map as well. Always use new keyword
+		
+		if (map.get(last_character)!= null) {
+		
+		}
+		HashSet<String> options = new HashSet<String>();
+		options.addAll(map.get(last_character));
+		
 
 		try {
 
-			options.removeAll(chain);
+			options.removeAll(chain); 
+			//this will remove values from original map it not created using new. 
+			
 
 			if (options.isEmpty()) {
 
@@ -123,11 +258,11 @@ public class Exercise45 {
 
 		ArrayList<ArrayList<String>> allWordsMaxes = new ArrayList<ArrayList<String>>();
 
-		for (String i : pokemonArray) {
+		for (String w : pokemonArray) {
 
 			ArrayList<String> word = new ArrayList<String>();
 
-			word.add(i);
+			word.add(w);
 
 			allWordsMaxes.add(find(word));
 
@@ -136,6 +271,37 @@ public class Exercise45 {
 		ArrayList<String> longest = allWordsMaxes.stream().max(Comparator.comparingInt(ArrayList::size)).get();
 
 		System.out.println(longest);
+		
+		System.out.println(longest.size());
 
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
